c2c95457d821ddf920bd0f242ff3129286b74877
Alexander Ponomarenko <brain16383@yandex.ru>
2022-12-04 22:40:17 +0300

    [createpack] Write .bat files in win1251 codepage

--- createPack/src/ports/FsOutput.ts
+++ createPack/src/ports/FsOutput.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
+import { unicodeToWin1251 } from './unicodeToWin1251';
 
 export class FsOutput {
     constructor() {}
 
     writeFile = (fName: string, data: string): Promise<void> => {
-        return fs.promises.writeFile(fName, data);
+        const data1251 = unicodeToWin1251(data);
+        return fs.promises.writeFile(fName, data1251);
     };
 }
